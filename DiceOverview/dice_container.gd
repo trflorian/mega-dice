4d95accbcc0e2901aef9e50823984f30aa50686d
trflorian <florian.trautweiler@hispeed.ch>
2023-12-30 22:47:45 +0100

    save and load dices

--- DiceOverview/dice_container.gd
+++ DiceOverview/dice_container.gd
@@ -7,7 +7,11 @@ class_name DiceContainer
 
 @onready var dice_container_parent: Control = %Dices
 
+var save_path = "user://dices.save"
+
 func _ready() -> void:
+	if Dice.dices.is_empty():
+		_load_dices()
 	_update_dices()
 
 func add_dice(name: String):
@@ -17,6 +21,8 @@ func add_dice(name: String):
 	
 	Dice.dices.append(dice_config)
 	_update_dices()
+	
+	edit_dice(dice_config)
 
 func remove_dice(dice_config: DiceConfig):
 	Dice.dices.remove_at(Dice.dices.find(dice_config))
@@ -37,3 +43,25 @@ func _update_dices():
 		dice_inst.set_config(dice)
 		dice_inst.on_remove.connect(remove_dice.bind(dice))
 		dice_inst.on_edit.connect(edit_dice.bind(dice))
+	_save_dices()
+
+func _save_dices():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	var dices_dict = {}
+	for dice in Dice.dices as Array[DiceConfig]:
+		dices_dict[dice.dice_name] = dice.dice_dots
+	file.store_string(JSON.stringify(dices_dict))
+	print("saved")
+
+func _load_dices():
+	if FileAccess.file_exists(save_path):
+		print("save file found")
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		var dices_dict = JSON.parse_string(file.get_as_text()) as Dictionary
+		for dice_name in dices_dict.keys():
+			var dice_cfg = DiceConfig.new()
+			dice_cfg.dice_name = dice_name
+			dice_cfg.dice_dots = dices_dict[dice_name] as Array[int]
+			Dice.dices.append(dice_cfg)
+	else:
+		print("no save file not found")
